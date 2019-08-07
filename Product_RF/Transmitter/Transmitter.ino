9fe00dcf856607ce327086ac641e8b718ec4751a
Rifki Ramadhani <51043477+rifkirdhani11@users.noreply.github.com>
2019-08-07 14:31:14 +0700

    Update Transmitter.ino

--- Product_RF/Transmitter/Transmitter.ino
+++ Product_RF/Transmitter/Transmitter.ino
@@ -1,38 +1,68 @@
-/*
-* Arduino Wireless Communication Tutorial
-*     Example 1 - Transmitter Code
-*                
-* by Dejan Nedelkovski, www.HowToMechatronics.com
-* 
-* Library: TMRh20/RF24, https://github.com/tmrh20/RF24/
-*/
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
 #include "DHT.h"
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
 
 #define DHTPIN 2 
 #define DHTTYPE DHT11
 
-RF24 radio(7, 8); // CE, CSN
+RF24 radio(10, 8); // CE, CSN
 DHT dht(DHTPIN, DHTTYPE);
+LiquidCrystal_I2C lcd(0x3F, 16, 2);
 
-const byte address[6] = "00001";
+const byte address[6] = "00002";
+const int button = 6;
+
+int butState;
+
 void setup() {
   Serial.begin(9600);
+  lcd.begin();
+  lcd.backlight();
+  lcd.clear();
+  lcd.setCursor(0,0);
+  dht.begin();
   
   radio.begin();
+  radio.setChannel(1);
   radio.openWritingPipe(address);
   radio.setPALevel(RF24_PA_MIN);
+  radio.setDataRate(RF24_250KBPS);
+  radio.setCRCLength(RF24_CRC_DISABLED);
   radio.stopListening();
+  radio.powerUp();
 
-  dht.begin();
+  pinMode(button, INPUT_PULLUP);
 }
+
+struct MessageData
+{   float temp;
+    unsigned char button;
+};
+
 void loop() {
-  float t = dht.readTemperature();
-  Serial.println(t);
-  
-  //const char text[] = "Hello World";
-  radio.write(&t, sizeof(t));
-  delay(1000);
+  MessageData pm;
+  pm.temp = dht.readTemperature();
+  pm.button = (unsigned char) digitalRead(button);
+
+  Serial.println(pm.temp);
+  Serial.println(pm.button);
+    
+  lcd.setCursor(0,0);
+  lcd.print(String("Temp(C) =")+String(pm.temp));
+
+  if (pm.temp>=30) {
+    lcd.setCursor(0,1);
+    lcd.print("Tekan 3 detik");
+ 
+    if (pm.button==0){
+      lcd.clear();
+    }
+  }
+    
+  radio.write(&pm, sizeof(pm));
+  delay(100);
+  lcd.clear();
 }
